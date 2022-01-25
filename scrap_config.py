e3171e79292ecf056fc0a75c0cc73a572dc64a1c
rifat <frifat2013@gmail.com>
2022-01-26 01:26:59 +0600

    Completed backbone

---
+++ scrap_config.py
@@ -0,0 +1,5 @@
+# keyword file must be csv
+# 'keyword' column must be present in the csv file (case does not matter)
+keyword_file_path = r"google_dk__all-keywords_2022-01-23_11-32-06-2.csv"
+done_keywords_csv_path = r"done_keywords.csv"
+domain_extentions = ["nu", "com", "net", "dk"]
