e71c0ce54a86b477712605a140215c25ff9b7142
rifat <frifat2013@gmail.com>
2022-01-30 18:43:02 +0600

    Updated config file

--- scrap_config.py
+++ scrap_config.py
@@ -1,15 +1,17 @@
+import secret_keys
+
 # keyword file must be csv
 # 'keyword' column must be present in the csv file (case does not matter)
 keyword_file_path = r"domains-to-process\pending.csv"
 done_keywords_csv_path = r"donekeywords\done_keywords.csv"
 domain_extentions = ["nu", "com", "net", "dk"]
-#domain_extentions = ["dk"]
+# domain_extentions = ["dk"]
 keep_record_of_keywords = True
 exclude_done_keywords = False
 drive_path = r"drive\chromedriver"
-api_key = "LXjGYlJ6w3aVrCgNFIhdPeUQ"
-api_name = "synergy"
-taken_status=0
-available_status=1
-unknown_status=2
-exception_status=-1
+api_key = secret_keys.api_key
+api_name = secret_keys.api_name
+taken_status = 0
+available_status = 1
+unknown_status = 2
+exception_status = -1
